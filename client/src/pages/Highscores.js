0f394816a18b3c0598991c8db3b9f37447cfc1a6
molly soufrine <mollysoufrine@gmail.com>
2020-10-13 18:54:41 -0400

    trying to work on the outline/basis for DB and retreiving info or inserting info to/from it. going to set up the model/schema. also made folders/files to follow the mern outline

--- client/src/pages/Highscores.js
+++ client/src/pages/Highscores.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class Highscore extends Component {
-
-  render() {
-    return (
-      <div>
-          <h1>Highscore</h1>
-       </div>
-    );
+function ShowHighscores() {
+  const [scores, setHighscores] = useState([]);
+  useEffect(() => {
+    loadHighscores();
+  }, []);
+  function loadHighscores() {
+    API.getHighScores().then((res) => setHighscores(res.data));
   }
+
+  return (
+    <div>
+      <h1>
+        {" "}
+        Here's a list of all the highscores and the players who earned them!{" "}
+      </h1>
+    </div>
+  );
 }
 
-export default Highscore;
+export default ShowHighscores;
