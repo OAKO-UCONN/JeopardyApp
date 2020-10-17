56aa7066c46aed7a7cedc7b18723133d201b9477
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-17 11:42:21 -0400

    fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const mongoose = require('mongoose')
 const app = express();
 const mongoose = require("mongoose")
 
