a8b6f177dc6e4e441093674eb1aba5487a4f768c
Pihonak18 <64816908+Pihonak18@users.noreply.github.com>
2020-10-17 13:28:10 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Game from "./pages/Game";
 // import Highscores from "./pages/Highscores";
 import About from "./pages/About";
 import Buttons from "./components/Buttons";
+import "./pages/style.css";
 
 function App() {
   return (
