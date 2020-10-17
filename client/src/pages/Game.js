195b2f1823a8181e1be9e822f7fc6a60a11624c0
molly soufrine <mollysoufrine@gmail.com>
2020-10-17 12:15:12 -0400

    removed the extra buttons from button component

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -198,6 +198,74 @@ function Game() {
           </div>
         </div>
       </div>
+
+      <div className="giveup">
+        <header>JEOPARDY!</header>
+      </div>
+      <div className="btn-group">
+        <div>
+          <button>Cat1</button>
+        </div>
+        <div>
+          <button>Cat2</button>
+        </div>
+        <div>
+          <button>Cat3</button>
+        </div>
+        <div>
+          <button>Cat4</button>
+        </div>
+      </div>
+      <div className="btn-group">
+        <button id="demo" onclick="myFunction()">
+          1
+        </button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+      <div className="btn-group">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+      <div className="btn-group">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+      <div className="btn-group">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+
+      <div className="card-group">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Player</h5>
+            <p className="card-text">Score:</p>
+            <button className="btn">Give Up</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Player</h5>
+            <p className="card-text">Score:</p>
+            <button className="btn">Give Up</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Player</h5>
+            <p className="card-text">Score:</p>
+            <button className="btn">Give Up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
