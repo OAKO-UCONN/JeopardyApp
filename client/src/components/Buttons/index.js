82ce1b963879193422b64fb5fd9879a47adb18d4
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-17 12:03:52 -0400

    Refactor index.js

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -2,33 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
+//Buttons This are the Buttons that show up on all pages.
 function Buttons() {
   return (
     <div class="jumbotron jumbotron-fluid">
       <div class="container buttons-container">
-      <Link className="btn btn-outline-primary" to="/">
-        Home
-      </Link>
-      <Link
-        to="/game"
-        className="btn btn-outline-info"
-      >
-       Play!
-      </Link>
-      <Link
-        to="/highscores"
-        className="btn btn-outline-success"
-      >
-        Scores
-      </Link>
-      <Link
-        to="/about"
-        className="btn btn-outline-danger"
-      >
-        About
-      </Link>
-    </div>
-    </div>
+
+        {/*Home Button*/}
+        <Link className="btn btn-outline-primary" to="/">Home</Link>
+
+        {/*Play Button*/}
+        <Link to="/game" className="btn btn-outline-info">Play!</Link>
+      
+        {/*Scores Button*/}
+        <Link to="/highscores" className="btn btn-outline-success"> Scores </Link>
+      
+        {/*About Button*/}
+        <Link to="/about"className="btn btn-outline-danger">About</Link>
+
+      </div>{/*BootStrap Container ENDS*/}
+    </div>//BootStrap jumbotron div ENDS
   );
 }
 
