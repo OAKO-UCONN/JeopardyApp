3b8999b5ba43d84b3cbb4412bb9a218f5d64875a
molly soufrine <mollysoufrine@gmail.com>
2020-10-19 17:02:52 -0400

    working on timer and how to disable modals after being clicked

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,35 +1,17 @@
 import React from "react";
-import questions from "./questions.json";
 
 function Game() {
-  const [showModal, setShowModal] = React.useState(false);
   let time = 15;
   const Timer = () => {
     setInterval(function countdown() {
       time -= 1;
       if (time === 0) {
         //close out question
-        setShowModal(false);
       }
     }, 1000);
   };
-
-  //questions array of objs...attributes wasclicked boolean,false by default, if true disable
-  //set diasble t
   return (
     <div className="wrapper">
-      {/* {questions.map(({ id, question, score, wasClicked }, index) => (
-        <div key={id}>
-          <button
-            disabled={wasClicked}
-            onClick={() => {
-              questions[index].wasClicked = true;
-            }}
-          >
-            {score}
-          </button>
-        </div>
-      ))} */}
       <div className="game-wrapper">
         <button className="button">Category</button>
         <button className="button" data-toggle="modal">
@@ -189,43 +171,42 @@ function Game() {
         </div>
       </div>
 
-      {showModal ? (
-        <div
-          class="modal fade"
-          id="exampleModal"
-          tabindex="-1"
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-        >
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
-                  Question Title
-                </h5>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">Question Data.</div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
-              </div>
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Question Title
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">Question Data.</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
             </div>
           </div>
         </div>
-      ) : null}
+      </div>
     </div>
   );
 }
