4c42b25eb382f28bbd41b0c5077c115abe53d177
molly soufrine <mollysoufrine@gmail.com>
2020-10-19 12:53:46 -0400

    added an update function to plaerysScores controllers...maybe some way if the same person plays again it would update their highscore?

--- controllers/playersScores.js
+++ controllers/playersScores.js
@@ -17,4 +17,9 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  update: function (req, res) {
+    db.Players.findOneandUpdate({ _id: res.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
