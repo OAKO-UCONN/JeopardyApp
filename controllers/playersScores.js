6f2ca7578ebcd5ac72e930e68496607ed9b3069d
molly soufrine <mollysoufrine@gmail.com>
2020-10-19 12:03:38 -0400

    working on controller file for getting all of the scores from the db

--- controllers/playersScores.js
+++ controllers/playersScores.js
@@ -1,6 +1,10 @@
 const db = require("../models");
 module.exports = {
   finadAll: function (req, res) {
-    db.Players.find(req.query);
+    db.Players.find(req.query)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        res.status(422).json(err);
+      });
   },
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +11,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// mongoose.connect("mongodb://localhost/gamerinfo");
+mongoose.connect("mongodb://localhost/scores");
 // Define API routes here
 
 // Send every other request to the React app
